5e1475dfa94c790a90f0f8dc64d5c62db47b6f3d
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-06-21 20:52:33 +0600

    Image component added but blur is not working properly

--- src/app/(pages)/service/page.jsx
+++ src/app/(pages)/service/page.jsx
@@ -1,8 +1,81 @@
+import Image from 'next/image';
+import wt_image from '../../../../public/doraemon.jpg';
 import style from './service.module.css';
 
 const Service = () => {
     return (
-        <h1 className={style.common_heading}>This is service page.</h1>
+        <div className='container mx-auto px-6 lg:px-8'>
+            <h1 className={style.common_heading}>Our Team</h1>
+
+            <div className='grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 py-14'>
+                {/* Member 01 */}
+                <div className='rounded-lg text-center px-5 py-14 bg-purple-100'>
+                    <div className='w-24 h-24 rounded-full mx-auto flex items-center justify-center bg-amber-500'>
+                        <Image src="/doraemon.jpg" width={150} height={150} alt='Team Member' className='h-full w-full rounded-full' />
+                    </div>
+
+                    <div className='text-center mt-5'>
+                        <h1 className='text-lg font-medium'>Walidur Tanjim</h1>
+                        <p className='font-medium text-gray-500'>Front End Developer</p>
+                    </div>
+                </div>
+
+                {/* Member 02 */}
+                <div className='relative rounded-lg text-center bg-purple-100'>
+                    <div className='w-full h-full rounded-full mx-auto flex items-center justify-center '>
+                        <Image src={wt_image} alt='Team Member' property='false' fill={true} quality={100} placeholder='blur' blurDataURL="..." className='rounded-lg'/>
+                    </div>
+                </div>
+
+                {/* Member 03 */}
+                <div className='rounded-lg text-center px-5 py-14 bg-purple-100'>
+                    <div className='w-24 h-24 rounded-full mx-auto flex items-center justify-center bg-amber-500'>
+                        <span className='text-xl font-medium text-white'>WT</span>
+                    </div>
+
+                    <div className='text-center mt-5'>
+                        <h1 className='text-lg font-medium'>Walidur Tanjim</h1>
+                        <p className='font-medium text-gray-500'>Front End Developer</p>
+                    </div>
+                </div>
+
+                {/* Member 04 */}
+                <div className='rounded-lg text-center px-5 py-14 bg-purple-100'>
+                    <div className='w-24 h-24 rounded-full mx-auto flex items-center justify-center bg-amber-500'>
+                        <span className='text-xl font-medium text-white'>WT</span>
+                    </div>
+
+                    <div className='text-center mt-5'>
+                        <h1 className='text-lg font-medium'>Walidur Tanjim</h1>
+                        <p className='font-medium text-gray-500'>Front End Developer</p>
+                    </div>
+                </div>
+
+                {/* Member 05 */}
+                <div className='rounded-lg text-center px-5 py-14 bg-purple-100'>
+                    <div className='w-24 h-24 rounded-full mx-auto flex items-center justify-center bg-amber-500'>
+                        <span className='text-xl font-medium text-white'>WT</span>
+                    </div>
+
+                    <div className='text-center mt-5'>
+                        <h1 className='text-lg font-medium'>Walidur Tanjim</h1>
+                        <p className='font-medium text-gray-500'>Front End Developer</p>
+                    </div>
+                </div>
+
+                {/* Member 06 */}
+                <div className='rounded-lg text-center px-5 py-14 bg-purple-100'>
+                    <div className='w-24 h-24 rounded-full mx-auto flex items-center justify-center bg-amber-500'>
+                        <span className='text-xl font-medium text-white'>WT</span>
+                    </div>
+
+                    <div className='text-center mt-5'>
+                        <h1 className='text-lg font-medium'>Walidur Tanjim</h1>
+                        <p className='font-medium text-gray-500'>Front End Developer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
